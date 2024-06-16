0edb1cb07d8c448cfb1396aec2bfe954e1b94c0c
ROHIT KUMAR TIWARI <145182326+rohitKT-23@users.noreply.github.com>
2024-06-16 19:55:18 +0530

    Time: 8 ms (74.49%), Space: 43.3 MB (87.63%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
             int sgn = 1;
             int num = 0;
-            Stack<Integer> stack = new Stack<>();
+            Stack<Integer> skt = new Stack<>();
 
             for (int str = 0; str < s.length(); str++) {
                 char chr = s.charAt(str);
@@ -22,16 +22,16 @@ class Solution {
                     sgn = -1;
 
                 } else if (chr == '(') {
-                    stack.push(ans);
-                    stack.push(sgn);
+                    skt.push(ans);
+                    skt.push(sgn);
                     ans = 0;
                     sgn = 1;
 
                 } else if (chr == ')') {
                     ans += sgn * num;
                     num = 0;
-                    ans *= stack.pop();
-                    ans += stack.pop();
+                    ans *= skt.pop();
+                    ans += skt.pop();
                 }
             }
 
