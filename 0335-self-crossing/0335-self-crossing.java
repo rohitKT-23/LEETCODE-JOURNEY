5eb112478f33d867dc661380d266d46e8db0b59d
ROHIT KUMAR TIWARI <145182326+rohitKT-23@users.noreply.github.com>
2024-07-31 15:15:09 +0530

    Time: 6 ms (56.8%), Space: 49.3 MB (68.64%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+
+        if (distance.length < 4) {
+            return false;
+        }
+
+        for (int i = 3; i < distance.length; i++) {
+
+            if (
+                distance[i] >= distance[i - 2] 
+                && 
+                distance[i - 1] <= distance[i - 3]
+                ) {
+                return true;
+            }
+
+            if (
+                i >= 4 
+                && 
+                distance[i - 1] == distance[i - 3] 
+                &&
+                distance[i] + distance[i - 4] >= distance[i - 2]
+                ) {
+
+                return true;
+            }
+            if (
+                i >= 5 
+                &&
+                distance[i - 2] >= distance[i - 4] 
+                && 
+                distance[i] + distance[i - 4] >= distance[i - 2] 
+                && 
+                distance[i - 1] <= distance[i - 3] 
+                && 
+                distance[i - 1] + distance[i - 5] >= distance[i - 3]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
