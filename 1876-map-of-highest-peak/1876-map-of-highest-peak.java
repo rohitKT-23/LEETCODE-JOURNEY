cd4ac60800b66b6d43a67145cf2ac0a6c18b37fc
ROHIT KUMAR TIWARI <145182326+rohitKT-23@users.noreply.github.com>
2025-01-22 21:15:49 +0530

    Time: 51 ms (55.05%), Space: 167.7 MB (43.43%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.java
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -2,11 +2,9 @@ class Solution {
     public int[][] highestPeak(int[][] isWater) {
         int m = isWater.length;
         int n = isWater[0].length;
-        
         int[][] res = new int[m][n];
         Queue<int[]> q = new LinkedList<>();
         boolean[][] visited = new boolean[m][n];
-        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(isWater[i][j] == 1){
@@ -16,18 +14,14 @@ class Solution {
                 }
             }
         }
-        
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
         while(!q.isEmpty()){
             int[] curr = q.poll();
             int x = curr[0];
             int y = curr[1];
-            
             for(int[] dir : dirs){
                 int newX = x + dir[0];
                 int newY = y + dir[1];
-                
                 if(newX >= 0 && newX < m && newY >= 0 && newY < n && !visited[newX][newY]){
                     res[newX][newY] = res[x][y] + 1;
                     visited[newX][newY] = true;
@@ -35,7 +29,6 @@ class Solution {
                 }
             }
         }
-        
         return res;
     }
 }
